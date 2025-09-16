fd08edfd18a4e45ca66aeb47b1b8282bf4a0eb6b
ayabakry <ayatbakry@yahoo.com>
2025-09-16 22:34:46 +0300

    fix bug about header and footer didn't appear and replace admin because used admin LTE

--- src/app/admin/page.jsx
+++
@@ -1,9 +0,0 @@
-function admin (){
-    return(
-         <div className="p-6">
-      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-      <p>Welcome, Admin 👋</p>
-    </div>
-    );
-}
-export default admin;
